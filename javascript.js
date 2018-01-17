0f89e83855ecbd2b5eef1d7982ff4ba14b0040b1
Antonella_Morittu <morittuantonella@gmail.com>
2018-01-17 15:38:04 +0100

    Javascript exercises #1

---
+++ javascript.js
@@ -0,0 +1,37 @@
+// Javascript Exercises #1
+// Create an array and add three numbers to it.
+var numbers = [5, 55 ,555];
+// Use your array to return the second number.
+numbers[1]
+// What data type is 123/12?
+var number = 123/12;
+// "Things in quotes!"?
+var string = "Hello World";
+// undefined?
+var undef = // Variable without a value. Value is undefined, type is undefined;
+
+// Write an if statement that returns true.
+var legalAge = 18;
+var canIbuyalcohol = function (myAge) {
+if (myAge >= legalAge) {
+return "True";
+}
+else {
+return "False";
+}
+};
+canIbuyalcohol(31);
+
+// Consider these two arrays: myArray = ['Thomas', 'Amber', 'Raoul']
+// and emptyArray = [].
+// Use a for loop to add our names to emptyArray.
+//  (Hint: n needs to be the length of the array.
+// Google a helper method for finding the length of an array in Javascript.
+// Is it the same as Ruby?)
+
+myArray = ['Thomas', 'Amber', 'Raoul']
+emptyArray = []
+
+for (var i=0; i<myArray.length; i++) {
+  emptyArray.push(myArray);
+}
